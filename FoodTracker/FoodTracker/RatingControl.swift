36b9fb5422005799ebbf1ff1c8e4f5e901a48a0b
Kakeru Nakabachi <b1015146@fun.ac.jp>
2017-05-30 08:48:30 +0900

    Implement a Custom Controlの途中までやった

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -13,10 +13,37 @@ class RatingControl: UIStackView {
     
    override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
     }
     
+    //MARK: Buton Action
+    
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed")
+    }
+
+    //MARK: Private Methods
+    
+    private func setupButtons() {
+        
+        //Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.blue
+        
+        //Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        //Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        //Add the button to the stack
+        addArrangedSubview(button)
+    }
 }
